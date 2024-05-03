f038d62943ee198f945beec00fae6fa94537d6e0
theazwy <theawenyizhu@gmail.com>
2024-05-03 15:35:17 -0400

    cleaning and visualizing stock market data

--- project.py
+++ project.py
@@ -1,2 +1,11 @@
-print("Welcome to the")
-print("jy sb")
+import yfinance as yf
+
+sp500 = yf.Ticker("^GSPC")
+sp500 = sp500.history(period="max")
+
+print(sp500)
+
+sp500.plot.line(y="Close", use_index=True)
+
+del sp500['Dividends']
+del sp500['Stock Splits']
