573eb4aefdd3a60510a510b0aa6e71af4f4088c8
theazwy <theawenyizhu@gmail.com>
2024-05-03 15:59:49 -0400

    mark the price as 0 or 1 as the price changes and delete data prior to 1990

--- project.py
+++ project.py
@@ -1,11 +1,20 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
+import pandas as pd
 
 sp500 = yf.Ticker("^GSPC")
 sp500 = sp500.history(period="max")
 
-print(sp500)
+# print(sp500)
 
-sp500.plot.line(y="Close", use_index=True)
+sp500.plot.line(y='Close', use_index=True)
+# plt.show()
 
 del sp500['Dividends']
 del sp500['Stock Splits']
+
+sp500['Tomorrow'] = sp500['Close'].shift(-1)
+sp500['Target'] = (sp500['Tomorrow'] > sp500['Close']).astype(int)
+
+sp500 = sp500.loc["1990-01-01":].copy()
+print(sp500)
