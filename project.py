c0635a09cc0afcf8e49151791166a21b96b5cb38
theazwy <theawenyizhu@gmail.com>
2024-05-03 16:14:27 -0400

    training an initial machine learning model

--- project.py
+++ project.py
@@ -1,15 +1,16 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score
 
 sp500 = yf.Ticker("^GSPC")
 sp500 = sp500.history(period="max")
 
-# print(sp500)
-
 sp500.plot.line(y='Close', use_index=True)
 # plt.show()
 
+# Cleaning the data
 del sp500['Dividends']
 del sp500['Stock Splits']
 
@@ -18,3 +19,13 @@ sp500['Target'] = (sp500['Tomorrow'] > sp500['Close']).astype(int)
 
 sp500 = sp500.loc["1990-01-01":].copy()
 print(sp500)
+
+model = RandomForestClassifier(n_estimators=100, min_samples_split=100, random_state=1)
+train = sp500.iloc[:-100]
+test = sp500.iloc[-100:]
+
+predictors = ['Close', 'Volume', 'Open', 'High', 'Low']
+model.fit(train[predictors], train['Target'])
+
+preds = model.predict(test[predictors])
+preds = pd.Series(preds, index = test.index)
